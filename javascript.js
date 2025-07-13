e126d24672d4c83fefca9c6cbeeaf1faf0f987cd
Md Mustak <mustakarman560@gmail.com>
2025-07-13 12:24:09 +0530

    Backend

---
+++ javascript.js
@@ -0,0 +1,8 @@
+//let x = 5;
+//for (let i = 0; i < x; i++) {
+ // console.log("Hello Mustaque ," + i);
+//}
+//console.log("bye Mustaque");
+
+const someValue = require("./math.js");
+console.log(someValue);
