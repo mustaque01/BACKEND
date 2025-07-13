76716b0d53af24b8017534eba3403cef4af4a86b
Md Mustak <mustakarman560@gmail.com>
2025-07-13 12:54:24 +0530

    ejs

--- EJSdir/index.js
+++ EJSdir/index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const port = 8080;
 
 
 app.set('view engine', 'ejs');
-app.get ('/', (req, res) => {
+app.set('views', path.join(__dirname, '/views'));
+app.get('/', (req, res) => {
   res.render('home.ejs', { title: 'Home Page' });
 });
 
+app.get('/about', (req, res) => {
+  res.send("Hello from the About Page");
+});
 
-app.listen (port, () => {
+
+app.get("/rolldice", (req, res) => {
+  let diceValue = Math.floor(Math.random() * 6) + 1;
+  res.render("rolldice.ejs", { num: diceValue });
+});
+
+app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
