45b36e9ca90ae4fc33e9ac07cb12a04f610ef6b6
MD-Mustak <mustakarman560@gmail.com>
2025-07-21 00:14:44 +0530

    mongoose

---
+++ MONGO/index.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+main()
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+        console.error('Error connecting to MongoDB:', err);
+    });
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/mydatabase', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+}
+
+const Schema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    email: String
+});
