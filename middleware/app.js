16fcb111936860d96cff30185e54e1e0fd35fe0b
MD-Mustak <mustakarman560@gmail.com>
2025-08-02 15:01:11 +0530

    add

--- middleware/app.js
+++ middleware/app.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const app = express();
 // middle ware ka kaam hota hai ki  next function ko call karna
-app.use((req, res) => {
-  let { query } = req.query;
-  console.log("Query parameters:", query); 
-  // Middleware logic can be added here
-  console.log("Middleware is running...");
-  res.send("Middleware response");
+// app.use((req, res, next) => {
+//   // let { query } = req.query;
+//   // console.log("Query parameters:", query); 
+//   // Middleware logic can be added here
+//   console.log("Middleware is running...");
+//   next();
+// });
+
+// // define second middleware
+// app.use((req, res, next) => {
+//   console.log("Second middleware is running...");
+//   next();
+//   // next ke baad code na likhe ye achhe coder ki nishani hai
+//   // ye next ki jagar return next(); likhna chahiye
+//   return next();
+// });
+
+// utility middleware use kia
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
 });
 
 app.get('/', (req, res) => {
