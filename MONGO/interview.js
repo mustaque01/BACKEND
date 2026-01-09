029d49fd6979f5581668bbc0b9a89a66a0fd7814
Md Mustak <mustakarman560@gmail.com>
2026-01-09 17:18:56 +0530

    Add MongoDB

---
+++ MONGO/interview.js
@@ -0,0 +1,5 @@
+//MongoDB is the most popular open-source, document-oriented NoSQL database, 
+// holding over 45% of the NoSQL market share. Unlike relational databases that
+//  rely on rigid tables, MongoDB stores data in a flexible BSON format
+//  (similar to JSON), enabling faster and more efficient storage and retrieval.
+
