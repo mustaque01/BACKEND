ce081aa3078eceda1e21473e100ee40551d25116
Md Mustak <mustakarman560@gmail.com>
2026-01-31 22:41:36 +0530

    Commit

--- javascript.js
+++
@@ -1,8 +0,0 @@
-//let x = 5;
-//for (let i = 0; i < x; i++) {
- // console.log("Hello Mustaque ," + i);
-//}
-//console.log("bye Mustaque");
-
-const someValue = require("./math.js");
-console.log(someValue);
--- math.js
+++
@@ -1,6 +0,0 @@
-const sum = (a, b) => a + b;
-const multiply = (a, b) => a * b;
-const g = 9.81;
-const PI = 3.14;
-
-Module.exports = "hello";
